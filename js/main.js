8cd20d14b70bb89372fa35f6eb1af0f341cc0ba3
Taiga-422 <taigakun0422@gmail.com>
2024-10-31 19:26:01 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 $("#hi-lo").click(function () {
     if (confirm('Hi and Low をPLAYしますか？')) {
-        window.location.href = "../hi-low.html";
+        window.location.href = "https://taiga-422.github.io/drinking-party-game/hi-low.html";
     }
 });
 
 $("#flash-w").click(function () {
     if (confirm('FLASH WORD をPLAYしますか？')) {
-        window.location.href = "../flash-w.html";
+        window.location.href = "https://taiga-422.github.io/drinking-party-game/flash-w.html";
     }
 });
 
 $("#janken").click(function () {
     if (confirm('じゃんけんゲームをPLAYしますか？')) {
-        window.location.href = "../janken.html";
+        window.location.href = "https://taiga-422.github.io/drinking-party-game/janken.html";
     }
 });
