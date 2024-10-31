24c05e5f4c67c1abd5759f27b297d2478f219416
Taiga-422 <taigakun0422@gmail.com>
2024-10-31 12:32:01 +0900

    JS初回課題

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$("#hi-lo").click(function () {
+    if (confirm('Hi and Low をPLAYしますか？')) {
+        window.location.href = "/hi-low.html";
+    }
+});
+
+$("#flash-w").click(function () {
+    if (confirm('FLASH WORD をPLAYしますか？')) {
+        window.location.href = "/flash-w.html";
+    }
+});
+
+$("#janken").click(function () {
+    if (confirm('じゃんけんゲームをPLAYしますか？')) {
+        window.location.href = "/janken.html";
+    }
+});
